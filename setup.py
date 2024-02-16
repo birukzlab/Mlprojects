19e74e39bf67b31263526803625803b2faca07f8
birukzlab <birukalemu63@gmail.com>
2024-02-15 18:29:06 -0600

    Data Ingestion

--- setup.py
+++ setup.py
@@ -4,18 +4,17 @@ from typing import List
 
 HYPEN_E_DOT = '-e .'
 def get_requirements(file_path:str)->List[str]:
-    
+
     '''
     This function will return the list of requirements
-
-
     '''
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-        [req.replace("\n", "") for req in requirements]
+        requirements = [req.replace("\n", " ") for req in requirements]
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
+
     return requirements
 
 
@@ -23,9 +22,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name="Mlprojects",
     version = '0.0.1',
-    author='Biruk',
+    author='birukzlab',
     author_email="birukalemu63@gmail.com",
     packages=find_packages(),
     install_requires = get_requirements('requirements.txt')
-
 )
